a88532a996d72ac1ec429c80e886f007bb96e6dc
Tomas Barrueto <tobarrueto@gmail.com>
2024-05-20 16:28:13 -0400

    ejercicio 0

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/tobarrueto.js
@@ -0,0 +1,36 @@
+
+//Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Representa las diferentes sintaxis que existen de crear comentarios
+
+/* 
+
+Comentario
+
+*/
+
+// Crea una variable (y una constante si el lenguaje lo soporta).
+
+let miVariable;
+
+const miConstante = "JavaScript!!!";
+
+
+//  Crea variables representando todos los tipos de datos primitivos
+
+// String (Texto)
+let varibleTexto = "Varible tipo String";
+
+// Number (Numero)
+let variableNumber = 7;
+
+// Boolean (Verdadaro o Falso)
+let variableBoolean = true;
+
+// Array (estructura que te permite almacenar varios valores en una sola referencia.)
+
+let variableArray = ["Tomás","Andrés","Barrueto","Barra"];
+
+console.log("Hola,", miConstante)
